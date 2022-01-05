9bab85832cd13d15983e2b8bc834b8051e6bed0c
unknown <akashp35@gmail.com>
2022-01-05 19:27:29 +0530

    some changes:

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -5,6 +5,8 @@ import fifteen from '../../images/shape/15.png'
 import thirteen from '../../images/shape/13.svg'
 import twel from '../../images/shape/12.svg'
 import './Service.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { serviceData } from '../../data/serviceData';
 const Service = () => {
     return (
         <div>
@@ -14,54 +16,20 @@ const Service = () => {
                         <h3>How We Can Help?</h3>
                     </div>
                     <div className="row">
-                        <div className="col-lg-4 col-md-6 col-sm-6">
+                        {
+                            serviceData.map((data,key)=>{
+                                return(
+                                    <div className="col-lg-4 col-md-6 col-sm-6">
                             <div className="single-services-box">
-                                <div className="icon bg-faddd4"><i className="flaticon-landing-page"></i></div>
-                                <h3><a href="single-service/index.html">Search Engine Optimization</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incutu
-                                    labore et dolore magna aliqua.</p>
+                                <div className={`icon ${data.cartColor}`}><i>{data.serviceIcon}</i></div>
+                                <h3><a href="single-service/index.html">{data.title}</a></h3>
+                                <p>{data.desc}</p>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-md-6 col-sm-6">
-                            <div className="single-services-box">
-                                <div className="icon bg-cafbf1"><i className="flaticon-goal"></i></div>
-                                <h3><a href="single-service/index.html">Social Media Strategy</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incutu
-                                    labore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6 col-sm-6">
-                            <div className="single-services-box">
-                                <div className="icon bg-ddd5fb"><i className="flaticon-contract"></i></div>
-                                <h3><a href="single-service/index.html">Real Time and Data</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incutu
-                                    labore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6 col-sm-6">
-                            <div className="single-services-box">
-                                <div className="icon bg-fcdeee"><i className="flaticon-application"></i></div>
-                                <h3><a href="single-service/index.html">Online Media Management</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incutu
-                                    labore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6 col-sm-6">
-                            <div className="single-services-box">
-                                <div className="icon bg-c5ebf9"><i className="flaticon-seo"></i></div>
-                                <h3><a href="single-service/index.html">Reporting &amp; Analysis</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incutu
-                                    labore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6 col-sm-6">
-                            <div className="single-services-box">
-                                <div className="icon bg-f8fbd5"><i className="flaticon-data-recovery"></i></div>
-                                <h3><a href="single-service/index.html">Penalty Recovery</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incutu
-                                    labore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
+                                )
+                            })
+                        }
+                        
                     </div>
                 </div>
                 <div className="default-animation">
---
+++ src/data/serviceData.jsx
@@ -0,0 +1,29 @@
+import mobile from '../images/services/mobile.svg'
+
+export const serviceData=[
+    {
+        cartColor:"bg-faddd4",
+        serviceIcon:mobile,
+        title:"Mobile Application Development",
+        desc:"Mobile Application Development Is The Cornerstone Of Any Modern Business. Our Experts Not Just Build And Test Technology That Is Inimitable But They Also Deploy And Support At Scale. Our Application Development Solutions Are Robust And Reliable, And We Always Use The Most Up-To-Date And Efficient UI/UX."
+    },
+    {
+        cartColor:"bg-cafbf1",
+        serviceIcon:"",
+        title:"Custom Software Development",
+        desc:"We Provide You The Several Off The Shelf Software Options Available On The Market, Their Out- Of The Box Features And Clunky Integration Capabilities Will Make For A Less Than Ideal Experience. We Assure That You Can Meet Your Business’s Unique Needs, Taking Into Consideration Every Nuance."
+    },
+    {
+        cartColor:"bg-ddd5fb",
+        serviceIcon:"",
+        title:"Web Application Development",
+        desc:"Rewaa Tech Verge Is A Highly Specialized Team For The Web Application Development And Services. Our Team Members Creates The Best Website, Which Will Be Fully Functional Very Secure And Able To Grow Your Business. And It Is Designed To Be User-Friendly."
+    },
+    {
+        cartColor:"bg-fcdeee",
+        serviceIcon:"",
+        title:"UI/UX Design",
+        desc:"Design Comes First And Always. As A Creative Agency, We Dream Up Engaging, Addictive User Experiences. We Make Apps Dynamic, Tested, And Pixel Perfect. Beautifully Simple, And Simply Beautiful . We Deliver Quality Products That Stay Within The Predefined Budget."
+    }
+
+]
